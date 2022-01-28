d31c0d03af8061d324c6397c870c7c91bb2cd060
liufeilog <liu_feilong@126.com>
2022-01-28 21:55:16 +0800

    静态页面

---
+++ LearnPdf/data/data.js
@@ -0,0 +1,47 @@
+var postList =[
+  {
+      author:{
+        date:"Jan 28 2020",
+        authorImage:"/images/avatar/avatar-5.png"
+      },
+      title:"小时候的冰棍儿与雪糕",
+      postImage:"/images/post/post-4.jpg",
+      content:"冰棍儿与雪糕决不是同一个东西。3到5毛的雪糕犹如现在的哈根达斯，5分到1毛的冰棍儿就像现在的老冰棒。时过境迁，...",
+      readingNum:92,
+      collectionNum:{
+        array:[108]
+      },
+      commentNum:7
+    },
+    {
+      author:{
+        date:"Jan 2 2022",
+        authorImage:"/images/avatar/avatar-1.png"
+      },
+      title:"从童年呼啸而过的火车",
+      postImage:"/images/post/post-5.jpg",
+      content:"小时候，家的后面有一条铁路。听说从男方北上的火车都要经过这条铁路。火车大多都是在晚上经过，但也不定是只有在夜深人静的时候，火车的声音才能从远方传来...",
+      readingNum:92,
+      collectionNum:{
+        array:[108]
+      },
+      commentNum:7
+    },
+    {
+      author:{
+        date:"Jan 28 2017",
+        authorImage:"/images/avatar/avatar-3.png"
+      },
+      title:"记忆里的春节",
+      postImage:"/images/post/post-3.jpg",
+      content:"年少时，有几样东西，是春节必不可少的：烟花、新衣、凉菜、压岁钱、饺子。年分大小年，有的地方是腊月二十三过小年，而有的地方是腊月二十四。。。",
+      readingNum:92,
+      collectionNum:{
+        array:[108]
+      },
+      commentNum:7
+    }]
+
+module.exports = {
+  postList : postList
+}
